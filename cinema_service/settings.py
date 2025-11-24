4bd028fda36206ef203ac8e20cd0b8f80a22763e
misha-cw <bigun.mykhaylo@gmail.com>
2025-11-24 20:48:41 +0200

    Add REST framework authentication settings and update URL patterns

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -136,3 +136,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ]
+}
